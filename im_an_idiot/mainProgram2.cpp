abfdfdb97889941b82595a9a489d908fa39ced4b
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2021-03-30 17:03:28 -0500

    Create mainProgram2.cpp

---
+++ im_an_idiot/mainProgram2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int CalcProgram(int firstNumber,int secondNumber,int Answer, char Operation) {
+	if (Operation == "+") {
+		if (firstNumber == "0") {
+			if (secondNumber == "0") {
+			
+			}
+		}
+	}
+}
