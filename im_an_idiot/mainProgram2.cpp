449aa588b24a20a1b06ff4313e72894925e51c1d
Zak <68399876+ZakGhost354313@users.noreply.github.com>
2021-03-30 17:41:18 -0500

    Update mainProgram2.cpp
    still has errors, i also need help

--- im_an_idiot/mainProgram2.cpp
+++ im_an_idiot/mainProgram2.cpp
@@ -4,12 +4,32 @@
 
 using namespace std;
 
-int CalcProgram(int firstNumber,int secondNumber,int Answer, char Operation) {
+void calcProgram(int firstNumber,int secondNumber,int Operation) {
+	int Answer 
 	if (Operation == "+") {
 		if (firstNumber == "0") {
 			if (secondNumber == "0") {
-			
+			    Answer = firstNumber + secondNumber;
+				cout << "Answer : "<< Answer << "\n";
 			}
 		}
 	}
 }
+
+int main() {
+	int firstNumber1;
+	int secondNumber1;
+	char Operation1;
+	
+	cout << "INPUT(firstNumber) : ";
+	cin >> firstNumber1;
+	cout << "\n";
+	cout << "INPUT(secondNumber) : ";
+	cin >> secondNumber1;
+	cout << "\n";
+	cout << "INPUT(Operation) : ";
+	cin >> Operation1;
+	cout << "\n";
+	
+	calcProgram(firstNumber1,secondNumber1,Operation1)
+    }
